7af0e812a1e4036d02520833fd46cadf919beb10
orderuniverse <hyx105304@163.com>
2022-07-04 18:07:52 +0800

    完成 federal register 项目开发

--- federal-register/src/main/java/com/ogog/federal/FederalRegisterStarter.java
+++ federal-register/src/main/java/com/ogog/federal/FederalRegisterStarter.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+/**
+ * <b>智慧医疗信息平台 - 注册中心启动类</b>
+ * @author orderuniverse
+ * @date 2022/7/4
+ */
 @EnableEurekaServer
+@SpringBootApplication
 public class FederalRegisterStarter {
 	public static void main(String[] args) {
 		SpringApplication.run(FederalRegisterStarter.class,args);
