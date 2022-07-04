3aa043bb24346423f8269b460bea28b98847b48f
orderuniverse <hyx105304@163.com>
2022-07-04 19:36:22 +0800

    完成 federal data Commons 信息

--- federal-data-commons/src/main/java/com/ogog/federal/pojo/entity/BaseEntity.java
+++ federal-data-commons/src/main/java/com/ogog/federal/pojo/entity/BaseEntity.java
@@ -1,7 +1,26 @@
 package com.ogog.federal.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
 /**
- * <b><b>
+ * <b>基础实体信息<b>
+ * @author orderuniverse
+ * @date 2022/7/4
  */
-public class BaseEntity {
+@Data
+public class BaseEntity implements Serializable {
+	private static final long serialVersionUID = -4470893021362849636L;
+	private String  status;                     // 系统状态：Y-启用，N-禁用
+	@TableField("createdBy")
+	private String createdBy;                   // 创建人
+	@TableField("createTime")
+	private Date createTime;                    // 创建时间
+	@TableField("modifiedBy")
+	private String modifiedBy;                  // 修改人
+	@TableField("modifiedTime")
+	private Date modifiedTime;                  // 修改时间
 }
